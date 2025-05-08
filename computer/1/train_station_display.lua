0c099190da4b03d9992d489b11c2ce874a68a5b7
Dario Ciceri <ciceri.dario.2002@gmail.com>
2025-05-08 22:41:45 +0200

    test con display e numero 14 verde o rosso su train signal

--- computer/1/train_station_display.lua
+++ computer/1/train_station_display.lua
@@ -1,43 +1,14 @@
-local modem = peripheral.find("modem") or error("Nessun modem trovato", 0)
-local CHANNEL = 14
-modem.open(CHANNEL)
-
 local monitor = peripheral.find("monitor") or error("Nessun monitor trovato", 0)
 monitor.setTextScale(0.5)
 monitor.clear()
 
-local number14 = {
-  "  #  ",
-  " # # ",
-  "   # ",
-  "   # ",
-  "   # ",
-  "   # ",
-  "   # "
-}
-
-local function drawNumber(color)
-  for i = 1, #number14 do
-    for j = 1, #number14[i] do
-      if number14[i]:sub(j, j) == "#" then
-        monitor.setCursorPos(j, i)
-        monitor.setBackgroundColor(color)
-        monitor.write(" ")
-      end
-    end
-  end
-end
-
-local function handleModemMessage(message)
-  if type(message) == "table" and message.type == "input_update" then
-    local color = redstone.getInput("right") and colors.red or colors.green
-    drawNumber(color)
-  end
-end
+local numero = "14"
+monitor.setCursorPos(1, 1)
 
 while true do
-  local event, side, channel, replyChannel, message, distance = os.pullEvent()
-  if event == "modem_message" and channel == CHANNEL then
-    handleModemMessage(message)
-  end
+  local statoRedstone = redstone.getInput("rigth")
+  local colore = statoRedstone and colors.red or colors.green
+  monitor.setTextColor(colore)
+  monitor.write(numero)
+  sleep(0.1)
 end
