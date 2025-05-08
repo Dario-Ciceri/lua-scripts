d722011474382c1ab1f76370ef94f0e634ea2dc5
Dario Ciceri <ciceri.dario.2002@gmail.com>
2025-05-08 22:18:04 +0200

    scomposto messaggio dai dati della table in display

--- computer/1/train_station_display.lua
+++ computer/1/train_station_display.lua
@@ -1,5 +1,6 @@
 local modem = peripheral.find("modem") or error("No modem attached", 0)
 local CHANNEL = 14
+
 modem.open(CHANNEL)
 
 local monitor = peripheral.find("monitor") or error("No monitor attached", 0)
@@ -11,8 +12,16 @@ local function displayMessage(msg)
   monitor.clear()
   monitor.setCursorPos(1, 1)
   monitor.write("Received message:")
-  monitor.setCursorPos(1, 2)
-  monitor.write(tostring(msg))
+  
+  if type(msg) == "table" and msg.type == "input_update" then
+    monitor.setCursorPos(1, 2)
+    monitor.write("Right Input: " .. tostring(msg.right))
+    monitor.setCursorPos(1, 3)
+    monitor.write("Left Input: " .. tostring(msg.left))
+  else
+    monitor.setCursorPos(1, 2)
+    monitor.write("Invalid message format")
+  end
 end
 
 while true do
