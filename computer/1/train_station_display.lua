9aaaf20cc9d92c9544eef831ed94c8e04f4409d4
Dario Ciceri <ciceri.dario.2002@gmail.com>
2025-05-08 23:22:57 +0200

    messo text color

--- computer/1/train_station_display.lua
+++ computer/1/train_station_display.lua
@@ -4,22 +4,26 @@ local CHANNEL = 14
 modem.open(CHANNEL)
 
 local monitor = peripheral.find("monitor") or error("No monitor attached", 0)
+
+monitor.setBackgroundColor(colors.black)
 monitor.clear()
 monitor.setCursorPos(1, 1)
-monitor.setTextScale(5) 
+monitor.setTextScale(15) 
 
 local function displayMessage(msg)
   
   if type(msg) == "table" and msg.type == "input_update" then
   
     if msg.right then
-        monitor.setBackgroundColor(colors.red)
+        monitor.setTextColor(colors.red)
         monitor.clear()
+        monitor.setCursorPos(1, 1)
         monitor.write("14")
         print(msg.right)
     else
-        monitor.setBackgroundColor(colors.green)
+        monitor.setTextColor(colors.green)
         monitor.clear()
+        monitor.setCursorPos(1, 1)
         monitor.write("14")
         print(msg.right)
     end
